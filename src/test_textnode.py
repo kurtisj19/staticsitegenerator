1b701e75b13b899d23d91da1cb18480804daf6f2
kurtisj19 <kurtis.johnson75@yahoo.com>
2024-04-24 20:25:23 -0400

    Implement unit tests

---
+++ src/test_textnode.py
@@ -0,0 +1,34 @@
+import unittest
+
+from textnode import TextNode
+
+
+class TestTextNode(unittest.TestCase):
+    def test_eq(self):
+        node = TextNode("This is a text node", "bold")
+        node2 = TextNode("This is a text node", "bold")
+        self.assertEqual(node, node2)
+
+    def test_eq2(self):
+        node = TextNode("This is a text node", "bold", None)
+        node2 = TextNode("This is a text node", "bold", None)
+        self.assertEqual(node, node2)
+    
+    def test_eq3(self):
+        node = TextNode("This is a text node", "bold", "https://www.google.com")
+        node2 = TextNode("This is a text node", "bold", "https://www.google.com")
+        self.assertEqual(node, node2)
+
+    def test_not_eq(self):
+        node = TextNode("This is a text node", "italic")
+        node2 = TextNode("This is a text node", "bold")
+        self.assertNotEqual(node, node2)
+    
+    def test_not_eq2(self):
+        node = TextNode("This is a text node", "bold")
+        node2 = TextNode("This is a text node", "bold", "https://www.google.com")
+        self.assertNotEqual(node, node2)
+
+
+if __name__ == "__main__":
+    unittest.main()
