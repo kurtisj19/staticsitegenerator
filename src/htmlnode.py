4af5fd32ca8797b383089c6c89b1a6aa1a9ea446
kurtisj19 <kurtis.johnson75@yahoo.com>
2024-05-07 21:53:00 -0400

    Add textnode to htmlnode helper func

--- src/htmlnode.py
+++ src/htmlnode.py
@@ -61,6 +61,29 @@ class ParentNode(HTMLNode):
         return result + f"</{self.tag}>"
 
 
+def text_node_to_html_node(text_node):
+    # Helper function to convert TextNode to HTMLNode
+    
+    text_types = ["text", "bold", "italic", "code", "link", "image"]
+
+    text_type = text_node.text_type
+    if text_type not in text_types:
+        raise Exception("Text node does not have valid text type")
+    
+    if text_type == "text":
+        return LeafNode(value=text_node.text)
+    elif text_type == "bold":
+        return LeafNode(tag="b", value=text_node.text)
+    elif text_type == "italic":
+        return LeafNode(tag="i", value=text_node.text)
+    elif text_type == "code":
+        return LeafNode(tag="code", value=text_node.text)
+    elif text_type == "link":
+        return LeafNode(tag="a", text=text_node.text, props={"href": text_node.url})
+    elif text_type == "image":
+        return LeafNode(tag="img", text="", props=
+                        {"src": text_node.url, "alt": text_node.text})
+
 if __name__ == "__main__":
     # node = HTMLNode(tag="a", value="a simple link", props={"href": "https://www.google.com", "target": "_blank"})
     # print(node.props_to_html())
